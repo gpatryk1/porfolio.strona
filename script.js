fac3049f1a263bb84975c1002d252241389bf5b0
pglanc <glancpatryk.1@gmail.com>
2024-01-22 17:03:47 +0100

    22

--- script.js
+++ script.js
@@ -43,6 +43,68 @@ window.onscroll = () => {
 
 }
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '200px',
+    duration: 2000,
+    reset: true     
+})
+
+  sr.reveal('.home-content',{})
+  sr.reveal('.topline',{delay: 400})
+  sr.reveal('.logo',{delay: 500,
+    origin: 'left',   
+    distance: '50px',})
+  sr.reveal('.icon-box',{delay: 500,
+    origin: 'left',   
+    distance: '100px',})
+  sr.reveal('',{delay: 200})
+  sr.reveal('.img-photo',{delay: 500,
+    origin: 'right',   
+    distance: '50px',})
+
+    // sr.reveal('.about',{delay: 300,
+    //     origin: 'left',   
+    //     distance: '50px',
+    //     distance: '500px'})
+
+        sr.reveal('.heading',{delay: 300,
+            origin: 'left',   
+            distance: '50px',
+            interval: 200})
+            
+        sr.reveal('.about-img',{delay: 300,
+            origin: 'right',   
+            distance: '100px',
+            })
+        sr.reveal('.about-content',{delay: 300,
+            origin: 'left',   
+            
+            distance: '300px'})
+        sr.reveal('.heading5',{delay: 300,
+            origin: 'left',   
+            
+            distance: '200px'})
+        sr.reveal('.container1',{delay: 300,
+            origin: 'right',   
+            distance: '200px'})
+        sr.reveal('.heading1',{delay: 300,
+               
+            distance: '300px'})
+        sr.reveal('.heading6',{delay: 300,
+               
+            distance: '200px'})
+        sr.reveal('.input-box',{delay: 300,
+            origin: 'left',   
+            distance: '300px'})
+        sr.reveal('.textarea-field',{delay: 300,
+            origin: 'right',   
+            distance: '500px'})
+        sr.reveal('.btn',{delay: 300,
+            origin: 'bottom',   
+            distance: '200px'})
+        
+
 
 
   // Ustaw domyślną głośność na 50% (możesz dostosować według potrzeb)
