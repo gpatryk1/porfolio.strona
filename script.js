2e6dee4ef315d7bf37e0b724df133bc8f761b1c2
pglanc <glancpatryk.1@gmail.com>
2024-01-24 12:30:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,18 @@ const sr = ScrollReveal({
 
   
 
+  import { Analytics } from '@vercel/analytics/react';
+ 
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          <title>Next.js</title>
+        </head>
+        <body>
+          {children}
+          <Analytics />
+        </body>
+      </html>
+    );
+  }
